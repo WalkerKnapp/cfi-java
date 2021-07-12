ea2a37a67507403fa1db3736c1924a6b6691944d
Walker Knapp <walker@walkerknapp.me>
2021-07-11 21:47:09 -0400

    Parse CMakeFiles Structure

--- src/main/java/me/walkerknapp/cfi/structs/CMakeFiles.java
+++ src/main/java/me/walkerknapp/cfi/structs/CMakeFiles.java
@@ -1,4 +1,38 @@
 package me.walkerknapp.cfi.structs;
 
+import com.dslplatform.json.CompiledJson;
+import com.dslplatform.json.JsonAttribute;
+
+import java.util.List;
+
 public class CMakeFiles implements CFIObject {
+    @JsonAttribute(mandatory = true)
+    public Paths paths;
+
+    @JsonAttribute(mandatory = true)
+    public List<Input> inputs;
+
+    @CompiledJson
+    public static class Paths {
+        @JsonAttribute(mandatory = true)
+        public String source;
+
+        @JsonAttribute(mandatory = true)
+        public String build;
+    }
+
+    @CompiledJson
+    public static class Input {
+        @JsonAttribute(mandatory = true)
+        public String path;
+
+        @JsonAttribute
+        public boolean isGenerated;
+
+        @JsonAttribute
+        public boolean isExternal;
+
+        @JsonAttribute
+        public boolean isCMake;
+    }
 }
