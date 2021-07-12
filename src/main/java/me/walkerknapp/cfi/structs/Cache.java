f69a03c48e95fb0ad43cf439a2507724399d664e
Walker Knapp <walker@walkerknapp.me>
2021-07-11 21:42:39 -0400

    Parse Cache Structure

--- src/main/java/me/walkerknapp/cfi/structs/Cache.java
+++ src/main/java/me/walkerknapp/cfi/structs/Cache.java
@@ -1,4 +1,36 @@
 package me.walkerknapp.cfi.structs;
 
+import com.dslplatform.json.CompiledJson;
+import com.dslplatform.json.JsonAttribute;
+
+import java.util.List;
+
+@CompiledJson
 public class Cache implements CFIObject {
+    @JsonAttribute(mandatory = true)
+    public List<Entry> entries;
+
+    @CompiledJson
+    public static class Entry {
+        @JsonAttribute(mandatory = true)
+        public String name;
+
+        @JsonAttribute(mandatory = true)
+        public String value;
+
+        @JsonAttribute(mandatory = true)
+        public String type;
+
+        @JsonAttribute(mandatory = true)
+        public List<Property> properties;
+
+        @CompiledJson
+        public static class Property {
+            @JsonAttribute(mandatory = true)
+            public String name;
+
+            @JsonAttribute(mandatory = true)
+            public String value;
+        }
+    }
 }
